fb5a69b6039f67f486029f3fa734a353091167f8
Thomas Lange <tigredepapier@gmx.de>
2018-06-30 11:51:47 +0200

    Finalization of "UpperCase"
    - Added a test line to make sure nothint in memory is overwritten
    - better documentatio

--- I32/Assembler_Linux_1/UpperCase.asm
+++ I32/Assembler_Linux_1/UpperCase.asm
@@ -4,20 +4,22 @@
 ;   Created date:           29.06.2018
 ;   Last update:            30.06.2018
 ;   Author:                 Thomas Lange
-;   Status:                 INCOMPLETE
+;   Status:                 DONE
 ;
-;   Short:      String to Upper Case
-;   Description:Load a string from memory and upper case it
+;   Short:          String to Upper Case
+;   Description:    Load a string from memory and upper case it
 ;
 ;   Instructions Used:
-;       MOV, CMP, SAL (Arithm. Left Shift)
-;       XOR, JLE, JGE
+;       MOV, CMP, (not used in the end) SAL (Arithm. Left Shift)
+;       XOR, JLE, JGE, JL, JG
 ;
 ;   Learned/Missconceptions/Errors:
 ;       Biggest missconception was the way memory is written to:
 ;       Instead of writing bytes, it wrote 4bytes
 ;
-;       This was fixed by: $$$ADD$$$
+;       This also meant that initaly i was changing much more then just the text
+;       Instead i did 8 times the character changes
+;       This is why I added a test line
 ;
 ;
 ;   Build Commands:
@@ -27,8 +29,11 @@
 
 
 SECTION .data
-Text:       db "this is Some teXt with wireD caSIng ! see&%$! what will HAP?N", 10
+Text:       db "this is Some teXt with wireD caSIng foo /&%%(/%/&%%(/%  bar   ", 10
 TxtLen:     equ $-Text
+Test:       db "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", 10, "this is the test that no other bytes are modified - all lower case", 10
+TestLen:    equ $-Test
+
 
 SECTION .bss
 
@@ -38,9 +43,8 @@ SECTION .text
 global _start
 
 _start:
-    nop             ; start of code
-
-    ; initial print of sentence
+    nop
+                                ; initial print of sentence
     mov eax, 4
     mov ebx, 1
 
@@ -48,32 +52,29 @@ _start:
     mov edx, TxtLen
     int 80H
 
-    ; intended upper casing
-    mov eax, Text
+start_upper_case:               ; start of upper casing
+    mov eax, Text               ; load the position and length into memory
     mov edx, TxtLen
-    sal edx, 3
-    mov ecx, 0
+    mov ecx, 0                  ; for the loop condition init the variable
 loop:
-    mov bl, byte [eax + ecx]
-    ; do check on modification here
+    mov bl, byte [eax + ecx]    ; now we read one byte at a time
 
-    cmp bl, 'a'
-    jl continue
+    cmp bl, 'a'                 ; check if byte less then 'a'
+    jl continue                 ; break if it is
 
-    cmp bl, 'z'
-    jg continue
+    cmp bl, 'z'                 ; check if greater then 'z'
+    jg continue                 ; break ir it is
 
-    ; modify to  upper case
-
+modify:                         ; modify to  upper case
     xor bl, 00100000b
-    mov [eax + ecx], byte bl
+    mov [eax + ecx], byte bl    ; write back one byte
 
-continue:       ; continue with execution and check if still in range of memory
-    inc ecx
+continue:                       ; continue with execution
+    inc ecx                     ; and check if still in range of memory
     cmp ecx, edx
-    jle loop
+    jl loop                     ; if we are still in range: jump to start of loop
 
-print:
+print:                          ; print (changed) sequence
     mov eax, 4
     mov ebx, 1
 
@@ -81,10 +82,18 @@ print:
     mov edx, TxtLen
     int 80H
 
+print_test:                     ; print controll sequence
+    mov eax, 4
+    mov ebx, 1
 
-stop:
+    mov ecx, Test
+    mov edx, TestLen
+    int 80H
+
+
+stop:                           ; gracefully exit (under linux)
     mov eax, 1
     mov ebx, 0
-    int 80H 
+    int 80H
 
-    nop             ; end of code
+    nop                         ; end of code
